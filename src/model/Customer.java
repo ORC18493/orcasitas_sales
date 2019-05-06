db8b0c6dd114bbd4e37bd9f40ad48826302ba2b4
stefanorcasitas1 <stefanorcasitas1@172.20.10.2>
2019-05-06 12:30:24 +0200

    Customer

---
+++ src/model/Customer.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Customer 
+
+{
+	private int customerID;
+	private String firstName;
+	
+	public Customer()
+	{
+		
+	}
+	 
+	public int getCustomerID() 
+	{
+		return customerID;
+	}
+	public String getFirstName() 
+	{
+		return firstName;
+	}
+	public void setCustomerID(int customerID) 
+	{
+		this.customerID = customerID;
+	}
+	public void setFirstName(String firstName) 
+	{
+		this.firstName = firstName;
+	}
+	
+	
+}
